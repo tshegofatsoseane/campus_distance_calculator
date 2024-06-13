9ef296594388a53a443498c9f2a8d5e1807f3c22
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-06-13 04:48:36 +0200

    Update tests

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -4,13 +4,16 @@ from distance_calculator.calculator import calculate_distance_and_time_to_campus
 class TestDistanceCalculator(unittest.TestCase):
 
     def test_calculate_distance_and_time_to_campuses(self):
-        street_address = "Your test address here"
+        street_address = "1600 Amphitheatre Parkway, Mountain View, CA"
         results = calculate_distance_and_time_to_campuses(street_address)
         self.assertIsNotNone(results)
-        self.assertIn('Mafikeng', results)
-        self.assertIn('driving', results['Mafikeng'])
-        self.assertIn('distance', results['Mafikeng']['driving'])
-        self.assertIn('duration', results['Mafikeng']['driving'])
+        self.assertIn('North-West University', results)
+        self.assertIn('University of Johannesburg', results)
+        self.assertIn('Mafikeng', results['North-West University'])
+        self.assertIn('Kingsway Campus', results['University of Johannesburg'])
+        self.assertIn('driving', results['North-West University']['Mafikeng'])
+        self.assertIn('distance', results['North-West University']['Mafikeng']['driving'])
+        self.assertIn('duration', results['North-West University']['Mafikeng']['driving'])
 
 if __name__ == '__main__':
     unittest.main()
