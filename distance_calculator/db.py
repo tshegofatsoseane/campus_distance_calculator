c5bca4ca2fa75893a755095c42b5eec5217976fd
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-06-13 06:45:21 +0200

    feat: implement function to update nearest campus

--- distance_calculator/db.py
+++ distance_calculator/db.py
@@ -14,9 +14,27 @@ def get_accommodations_with_null_campus(university):
     SELECT id, Street_Address
     FROM accommodations_accommodation
     WHERE Nearest_Campus IS NULL
-    AND Street_Address LIKE %s
+    AND university = %s
     """
-    cursor.execute(query, ('%' + university + '%',))
+    cursor.execute(query, (university,))
     results = cursor.fetchall()
     conn.close()
     return results
+
+def update_nearest_campus(accommodation_id, nearest_campus):
+    conn = mysql.connector.connect(
+        host=DB_CONFIG['HOST'],
+        user=DB_CONFIG['USER'],
+        password=DB_CONFIG['PASSWORD'],
+        database=DB_CONFIG['NAME'],
+        port=DB_CONFIG['PORT']
+    )
+    cursor = conn.cursor()
+    update_query = """
+    UPDATE accommodations_accommodation
+    SET Nearest_Campus = %s
+    WHERE id = %s
+    """
+    cursor.execute(update_query, (nearest_campus, accommodation_id))
+    conn.commit()
+    conn.close()
