07b20673615649b4e3e8ccac70d30fa44a057c62
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-06-18 03:56:39 +0200

    Refactr: fix accommodation street address column index

--- distance_calculator/campus_calculator.py
+++ distance_calculator/campus_calculator.py
@@ -1,7 +1,7 @@
 import click
 from sqlalchemy import create_engine, MetaData, Table, select
 from geopy.distance import geodesic
-from google_maps import get_geocode  # Assuming google_maps.py is in the same directory
+from google_maps import get_geocode  
 
 DB_ENGINE = 'mysql+pymysql'
 DB_NAME = 'AccommodationDB'
@@ -26,6 +26,7 @@ def find_nearest_campus(street_address, campuses):
     try:
         accommodation_location = get_geocode(street_address)
         if not accommodation_location:
+            print(f"Could not geocode address: {street_address}")
             return None
 
         accommodation_coords = accommodation_location[0].get('geometry', {}).get('location')
@@ -36,14 +37,17 @@ def find_nearest_campus(street_address, campuses):
 
         for campus_name, campus_address in campuses.items():
             campus_location = get_geocode(campus_address)
-            if campus_location:
-                campus_coords = campus_location[0].get('geometry', {}).get('location')
-                campus_latlng = (campus_coords['lat'], campus_coords['lng'])
-                distance = geodesic(accommodation_latlng, campus_latlng).kilometers
+            if not campus_location:
+                print(f"Could not geocode campus address: {campus_address}")
+                continue
 
-                if distance < shortest_distance:
-                    shortest_distance = distance
-                    nearest_campus = campus_name
+            campus_coords = campus_location[0].get('geometry', {}).get('location')
+            campus_latlng = (campus_coords['lat'], campus_coords['lng'])
+            distance = geodesic(accommodation_latlng, campus_latlng).kilometers
+
+            if distance < shortest_distance:
+                shortest_distance = distance
+                nearest_campus = campus_name
 
         return nearest_campus
 
@@ -58,7 +62,7 @@ def update_nearest_campus():
 
     for accommodation in results:
         try:
-            street_address = accommodation[3]  # Assuming 'Street_Address' is in position 4 (index 3)
+            street_address = accommodation[5] 
             nearest_campus = find_nearest_campus(street_address, UJ_CAMPUS_ADDRESSES)
             if nearest_campus:
                 print(f"Accommodation ID {accommodation[0]} - Nearest campus: {nearest_campus}")
